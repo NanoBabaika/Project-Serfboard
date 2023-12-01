24fff2ea51d6adba208f6aa576ab3d8c31fa1cf0
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-01 11:01:16 +0300

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,15 +1,42 @@
 
-$(".form").submit(e => {
-    e.preventDefault();
+// $(".form").submit(e => {
+//     e.preventDefault();
 
-    $.fancybox.open({
-		src: '#modal',
-		type: 'inline'
-	});
-});
+//     $.fancybox.open({
+// 		src: '#modal',
+// 		type: 'inline'
+// 	});
+// });
 
-$(".app-submit-btn").click(e => {
-    e.preventDefault();
+// $(".app-submit-btn").click(e => {
+//     e.preventDefault();
 
-    $.fancybox.close();
-})
+//     $.fancybox.close();
+// })
+
+
+
+// Запрос на JS
+
+
+// Открытие модального окна
+var modal = document.getElementById('modal');
+var btn = document.getElementById("ordersbtn");
+var span = document.getElementsByClassName("app-submit-btn")[0];
+
+btn.onclick = function (){
+    modal.style.display = "block";
+    return false;
+}
+
+span.onclick = function (){
+    
+    modal.style.display = "none";
+}
+
+
+window.onclick =function (event) {
+ if(event.target == modal) {
+     modal.style.display = "none";
+ }
+}
