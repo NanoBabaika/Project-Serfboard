47d1c0c541a5ac3be201ed0a939f35e7dd2afd75
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-06 22:32:41 +0300

    Add files via upload

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 let video;
 let durationControl;
 let soundControl;
-let interValid;
+let intervalId;
 
 document.addEventListener('DOMContentLoaded', e => {
   video = document.getElementById('video');
@@ -14,7 +14,87 @@ document.addEventListener('DOMContentLoaded', e => {
   for(let i=0; i < playButtons.length; i++) {
     playButtons[i].addEventListener('click', playStop);
   } 
+
+  // обработчик события на кнопку динамика
+  let micControl = document.getElementById('micLevel');
+  micControl.addEventListener('click, soundOf');
+
+  // обработчик события для ползунка продолжительности видео
+  durationControl = document.getElementById('durationLevel');
+  durationControl.addEventListener('mousedown', stopInterval);
+  durationControl.addEventListener('click', setVideoDuration);
+
+  durationControl.min = 0;
+  durationControl.value = 0;
+
+  // обработчик событя для ползунка громкости
+  soundControl = document.getElementById('volumeLevel');
+  soundControl.addEventListener('click', changeSoundVolume);
+  soundControl.addEventListener('mouseup', changeSoundVolume);
+
+  // Задаем максимальное и минимальное значение ползунка громкости
+  soundControl.min = 0;
+  soundControl.max = 10;
+
+  soundControl.value = soundControl.max;
 })
 
-// 37:14
- 
+
+function playStop(){
+  // находим кнопку с картинкой плей и скрываем ее
+  let playImg = document.querySelector('.video__play');
+  playImg.classList.toggle('.video__play--active');
+
+  // присваиваем ползунку продолжительности максимальное значение равное продолжительности видео
+  durationControl.max = video.duration;
+
+  // проверяем стоит ли видео на паузе, если да продолжаем воспроизведение
+  if(video.paused){
+    video.play();
+    intervalId = setInterval(updateDuration, 1000/66);
+  } else {
+    video.pause();
+    clearInterval(intervalId)
+  }
+
+}
+
+
+function updateDuraton(){
+  durationControl.value = video.currentTime;
+}
+
+function stopInterval(){
+  video.pause();
+  clearInterval(intervalId);
+}
+
+// Возможность перемотки видео
+function setVideoDuration(){
+  if(video.paused){
+    video.play();
+  } else{
+    video.pause();
+  }
+
+  // обновляем ползунок каждые 15 милисикунд
+  video.currentTime = durationControl.value;
+  intervalId = setInterval(updateDuraton, 1000/66);
+}
+
+// управление звуком
+function changeSoundVolume(){
+  video.volume = soundControl.value / 10;
+}
+
+// выключение звука
+function soundOf(){
+  if(video.volume ===0) {
+    video.volume = soundLevel;
+    soundControl.value = soundLevel * 10;
+  } else {
+    soundLevel = video.volume;
+    video.volume = 0;
+    soundControl.value = 0;
+  }
+}
