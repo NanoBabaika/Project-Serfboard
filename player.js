23b2a9182f5740f7b8858b7f2f733801f661fb28
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-06 17:57:38 +0300

    Add files via upload

--- player.js
+++ player.js
@@ -1,20 +1,20 @@
-let player;
+let video;
+let durationControl;
+let soundControl;
+let interValid;
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("yt-player", {
-    height: '392',
-    width: '662',
-    videoId: 'IaCzleuPb08&ab',
-    // events: {
-    //   'onReady': onPlayerReady,
-    //   'onStateChange': onPlayerStateChange
-    // },
-    // playerVars:{
-    //     controls:0,
-    //     disablekb:0,
-    //     showinfo:0,
-    //     rel:0,
-    //     autoplay:0
-    // }
-  });
-}
+document.addEventListener('DOMContentLoaded', e => {
+  video = document.getElementById('video');
+
+  // обработчик тега видео
+  video.addEventListener('click', playStop);
+
+  // Находим кнопки и вешаем цикл и обработчик на каждую
+  let playButtons = document.querySelectorAll('.play');
+  for(let i=0; i < playButtons.length; i++) {
+    playButtons[i].addEventListener('click', playStop);
+  } 
+})
+
+// 37:14
+ 
