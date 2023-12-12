7be4f46982a4be7bb134f1ab47411d12f5bf6c61
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-12 23:03:13 +0300

    Add files via upload

--- productsmenu.js
+++ productsmenu.js
@@ -2,9 +2,21 @@ const messureWidth = () => {
     return 500;
 }
 
+const closeEveryItemInContainer = container => {
+    const items = container.find(".products__menu");
+
+    const content = container.find(".products__menu-content");
+
+    items.removeClass("active");
+    content.width(0);
+}
+
 const openprodItem = item => {
     const hiddenContent = item.find(".products__menu-content");
     const reqWidth = messureWidth();
+
+    item.addClass("active");
+    hiddenContent.width(reqWidth);
 }
 
 $(".products__menu-title").on("click", e => {
@@ -12,6 +24,14 @@ $(".products__menu-title").on("click", e => {
 
     const $this = $(e.currentTarget);
     const item = $this.closest(".products__menu-item");
+    const itemOpened = item.hasClass("active");
+    const container = $this.closest(".products__menu");
 
-    openItem(item);
+    if(itemOpened){
+        closeEveryItemInContainer()
+    }else{
+        openprodItem(item);
+    }
+
+  
 });
