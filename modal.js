02eaed6e07e5a9635cb431f0c8fe174dee4e7fa6
Alexander <117478092+NanoBabaika@users.noreply.github.com>
2023-12-13 22:42:39 +0300

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,97 +1,41 @@
-const validateForm = (form, fieldsArray) => {
-
-    fieldsArray.forEach((field) =>{
-        field.removeClass("input-error");
-        if(field.val().trim() === "") {
-            field.addClass("input-error");
-        }
-    });    
-
-    const errorFields = form.find(".input-error");
-
-    return errorFields.length === 0;
-}
-
-
 $('.form').submit(e => {
     e.preventDefault();
 
     const form = $(e.currentTarget);
-    const name = form.find("[name ='name']");
-    const phone = form.find("[name ='phone']");
-    const comment = form.find("[name ='comment']");
-    const to = form.find("[name ='to']");
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
 
-    const modal = $("#modal");
-    const content = $(".modal__content");
+    [name, phone, comment,to].forEach(field =>{
 
-    modal.removeClass("error-modal");
+        if(field.val() === "") {
+            field.addClass("input-error");
+        }
+    });
 
-    const isValid = validateFields(form, [name, phone, comment, to]);
+    $.ajax({
+        url: "https://webdev-api.loftschool.com/sendmail",
+        method: "post",
+        data:{
+            name: name.val(),
+            phone:phone.val(),
+            comment:comment.val(),
+            to:to.val(),
+        }
+    })
 
-    if(isValid) {
-     const request = $.ajax({
-            url:"https://webdev-api.loftschool.com/sendmail",
-            method:"post",
-    
-            data: {
-                name:name.val(),
-                phone:phone.val(),
-                comment:comment.val(),
-                to:to.val(),
-            },
-
-
-        });
-
-        request.done((data) => {
-            content.text(data.massage);
-        });
-
-        request.fail((data) =>{
-            const message = data.resposeJSON.message;
-            content.text(message);
-            modal.addClass("error-modal");
-        });
-
-        request.always((data) => {
-            $.fancybox.open({
-                src: '#modal',
-                type: 'inline',
-            }); 
-        })
-    }
-     
+    $.fansybox.open ({
+        src:"#modal",
+        type:"inline"
+    })
 });
 
-
-
-$(".app-submit-btn").click(e => {
+$(".app-submit-btn").click(e =>{
     e.preventDefault();
 
-    $.fancybox.close();
+
+    $.fansybox.close();
 })
 
-
-
-// Открытие модального окна
-// var modal = document.getElementById('modal');
-// var btn = document.getElementById("ordersbtn");
-// var span = document.getElementsByClassName("app-submit-btn")[0];
-
-// btn.onclick = function (){
-//     modal.style.display = "block";
-//     return false;
-// }
-
-// span.onclick = function (){
-    
-//     modal.style.display = "none";
-// }
-
-
-// window.onclick =function (event) {
-//  if(event.target == modal) {
-//      modal.style.display = "none";
-//  }
-// }
+// 4:41
